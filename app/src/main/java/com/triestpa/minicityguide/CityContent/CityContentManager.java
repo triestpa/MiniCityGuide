65cdb850c935131b6069f116567054b7aa80a3ad
triestpa <triestpa@grinnell.edu>
2015-03-03 21:30:23 -0600

    Add city data csv file

--- app/src/main/java/com/triestpa/minicityguide/CityContent/CityContentManager.java
+++ app/src/main/java/com/triestpa/minicityguide/CityContent/CityContentManager.java
@@ -10,24 +10,19 @@ public class CityContentManager {
     private static ArrayList<City> citiesInstance;
 
     protected CityContentManager() {
-
     }
 
-    public ArrayList<City> getCities() {
+    public static ArrayList<City> getCities() {
         if(citiesInstance == null) {
             citiesInstance = new ArrayList<City>();
         }
         return citiesInstance;
     }
 
-    public void addCity(City city) {
+    public static void addCity(City city) {
         if (citiesInstance == null) {
             citiesInstance = new ArrayList<City>();
         }
-        else {
-            citiesInstance.add(city);
-        }
+        citiesInstance.add(city);
     }
-
-
 }
